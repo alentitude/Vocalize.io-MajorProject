f0aa50a6654c695ed0befb17fbd85390b48fc72e
alentitude <122140636+alentitude@users.noreply.github.com>
2024-12-24 11:20:30 +0530

    Added new components

--- vocalize.io/src/App.js
+++ vocalize.io/src/App.js
@@ -1,9 +1,10 @@
 import Home from "./components/home/Home";
+import Generate from "./components/generated/Generate";
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <Generate />
     </div>
   );
 }
